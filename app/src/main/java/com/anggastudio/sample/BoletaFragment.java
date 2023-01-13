27bd378c54ee126270873818031ebd1fe57dd0e4
Melina Llacua Guere <melina.llacua@gmail.com>
2023-01-12 22:02:21 -0500

    modal

--- app/src/main/java/com/anggastudio/sample/BoletaFragment.java
+++ app/src/main/java/com/anggastudio/sample/BoletaFragment.java
@@ -1,6 +1,9 @@
 package com.anggastudio.sample;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,19 +15,62 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class BoletaFragment extends DialogFragment {
 
 
+    private static android.content.Context Context;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_boleta, container, false);
+        View view = inflater.inflate(R.layout.fragment_boleta, container, false);
 
+        Button closeButton = view.findViewById(R.id.btncancelarboleta);
+        Button tipoPago = view.findViewById(R.id.btntipopago);
+        Button guardar = view.findViewById(R.id.btnagregarboleta);
+        Button generador = view.findViewById(R.id.btngenerarcliente);
+
+        TextView placa = view.findViewById(R.id.inputnplaca);
+        TextView dni = view.findViewById(R.id.inputdni);
+        TextView nombre = view.findViewById(R.id.inputnombre);
+        TextView direccion = view.findViewById(R.id.inputdireccion);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        tipoPago.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TipoPagoFragment dialog = new TipoPagoFragment();
+                dialog.show(getFragmentManager(), "Tipo de Pago");
+            }
+        });
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        generador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placa.setText("7845d78d");
+                nombre.setText("75994784");
+                nombre.setText("Jhon Pino");
+                nombre.setText("Jr. Unión");
+            }
+        });
+
+
+
+        return view;
     }
-
-
 
 }
--- app/src/main/java/com/anggastudio/sample/FacturaFragment.java
+++ app/src/main/java/com/anggastudio/sample/FacturaFragment.java
@@ -8,58 +8,40 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FacturaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FacturaFragment extends DialogFragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FacturaFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FacturaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FacturaFragment newInstance(String param1, String param2) {
-        FacturaFragment fragment = new FacturaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private static android.content.Context Context;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_factura, container, false);
+        View view = inflater.inflate(R.layout.fragment_boleta, container, false);
+
+        Button closeButton = view.findViewById(R.id.btncancelarfacturacion);
+        Button tipoPago = view.findViewById(R.id.btntipopago);
+        Button guardar = view.findViewById(R.id.btnagregarfacturacion);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        tipoPago.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TipoPagoFragment dialog = new TipoPagoFragment();
+                dialog.show(getFragmentManager(), "Tipo de Pago");
+            }
+        });
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+        return view;
     }
 }
