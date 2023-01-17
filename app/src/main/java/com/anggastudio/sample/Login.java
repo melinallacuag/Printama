1dcc62b4f00edbbf0893e3a91bae9ce981fc28bf
Melina <melina.llacua@gmail.com>
2023-01-17 10:04:37 -0500

    login

--- app/src/main/java/com/anggastudio/sample/Login.java
+++ app/src/main/java/com/anggastudio/sample/Login.java
@@ -15,26 +15,39 @@ import com.anggastudio.sample.mock.Mock;
 import com.anggastudio.sample.model.PrintBody;
 import com.anggastudio.sample.model.PrintHeader;
 import com.anggastudio.sample.model.PrintModel;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
-    Button btn_login;
-    EditText usuario, contraseña;
+    Button btniniciar;
+    TextInputEditText usuario, contraseña;
+    TextInputLayout alertuser,alertpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btn_login = findViewById(R.id.btnlogin);
-        usuario = findViewById(R.id.usuario);
-        contraseña = findViewById(R.id.contraseña);
-        btn_login.setOnClickListener(new View.OnClickListener() {
+
+        btniniciar     = findViewById(R.id.btnlogin);
+        usuario        = findViewById(R.id.usuario);
+        contraseña     = findViewById(R.id.contraseña);
+        alertuser      = findViewById(R.id.textusuario);
+        alertpassword  = findViewById(R.id.textcontraseña);
+
+        btniniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String usuarioUser = usuario.getText().toString().trim();
+                String usuarioUser    = usuario.getText().toString().trim();
                 String contraseñaUser = contraseña.getText().toString().trim();
 
-                if (usuarioUser.isEmpty() &&  contraseñaUser.isEmpty()){
-                    Toast.makeText(Login.this,"Ingresar datos",Toast.LENGTH_SHORT).show();
-                }else {
+                if (usuarioUser.isEmpty()){
+                    alertuser.setError("Ingresar usuario");
+                }else{
+                    alertuser.setErrorEnabled(false);
+                }
+                if (contraseñaUser.isEmpty()){
+                    alertpassword.setError("Ingresar contraseña");
+                }else{
+                    alertpassword.setErrorEnabled(false);
                     startActivity(new Intent(Login.this,Ventas.class));
                 }
             }
